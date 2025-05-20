69a9eaeb6c4f3aa0663798eda00fb10751bec354
nakanishikazuki <nakanishikazuki@localhost>
2025-05-20 23:39:17 +0900

    implemented Variables.java

---
+++ src/basicSyntax/Variables.java
@@ -0,0 +1,18 @@
+package basicSyntax;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int num1 = 57;
+		double num2 = 3.14;
+		String myfavorite = "私の好きな";
+		
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(myfavorite + "整数は" + num1 +"です。");
+		System.out.println(myfavorite + "小数は" + num2 + "です。");
+
+	}
+
+}
